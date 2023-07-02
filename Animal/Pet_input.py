12a7cde38ceb6bee12c37ebd16094b9b0a0c8189
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-02 20:44:04 +0800

    Added input fields

--- Animal/Pet_input.py
+++ Animal/Pet_input.py
@@ -3,4 +3,6 @@ pet=Pet
 
 default_pet=pet("", "", 0)
 default_pet.set_name(name = input("Put name here:"))
+default_pet.set_age(age = int(input("Input age here:")))
+default_pet.set_animal_type(animal_type=input("Please input pet type here:"))
 default_pet.get_name()
