55466d2b960b1dbdeaa5e78f273380ed196f6921
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-02 20:24:06 +0800

    Added the brake methods as well as the print in the for loop

--- Car/Test Car.py
+++ Car/Test Car.py
@@ -4,7 +4,12 @@ car = Car
 CarLoops = repeat
 
 Car1=car("Toyota Corolla", 2013, 0)
+
 for _ in range(5):
     Car1.accelerate()
 
 Car1.get_speed()
+
+for _ in range(5):
+    Car1.brake()
+    Car1.get_speed()
