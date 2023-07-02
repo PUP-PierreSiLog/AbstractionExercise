3fd2d868970bce55462202ffd846b16603e29465
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-02 20:32:50 +0800

    Did the first setters and getters

--- Animal/Pet.py
+++ Animal/Pet.py
@@ -3,3 +3,10 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
+    
+    def set_name(self, name):
+        name = input("Please input name here")
+        self.__name = name
+
+    def get_name (self):
+        return self.__name
